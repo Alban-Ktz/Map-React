5e6e7a5e56a6625318b127caa1cefe1fff343da6
Thomas <thomas.basquin2@gmail.com>
2022-02-22 22:15:08 +0100

    Transformation du projet avec cordova

--- testmap-withouttsx/src/index.js
+++ testmap-withouttsx/src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./containers/App/App";
+import registerServiceWorker from "./registerServiceWorker";
 
+let startApp = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+};
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (!window.cordova) {
+  startApp();
+} else {
+  document.addEventListener("deviceready", startApp, false);
+}
+
 registerServiceWorker();
