c2099b4b377127577ebf94faab44364154780f23
Alban Ktz <alban.ktz@gmail.com>
2022-02-22 22:17:55 +0100

    t

--- testmap-withouttsx/src/components/Map/Map.js
+++ testmap-withouttsx/src/components/Map/Map.js
@@ -15,14 +15,8 @@ class Map extends React.Component {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-<<<<<<< HEAD
         <Marqueur position={cathedrale} nom={'Cathédrale'} des={'salut'} />
 
-=======
-        
-        <Marqueur position={cathedrale} nom={'Cathédrale'} des = {'salut'}/>
-        
->>>>>>> 42fe43defc5e0e3a508a5f7a8feec35493275864
       </MapContainer>
       
 
