42fe43defc5e0e3a508a5f7a8feec35493275864
Nathan.C <coursnathan57@gmail.com>
2022-02-22 19:15:02 +0100

    Add Register

--- testmap-withouttsx/src/components/Map/Map.js
+++ testmap-withouttsx/src/components/Map/Map.js
@@ -5,7 +5,7 @@ import Marqueur from "../Marqueur/Marqueur";
 
 class Map extends React.Component {
   render() {
-
+    
     const position = [49.12038112160482, 6.1637997];
     const cathedrale = [49.1202, 6.1757];
 
@@ -15,6 +15,7 @@ class Map extends React.Component {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        
         <Marqueur position={cathedrale} nom={'Cathédrale'} des = {'salut'}/>
         
       </MapContainer>
