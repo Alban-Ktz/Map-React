2373e160719c6c352b8694477ab95ffea123c5ec
Alban Ktz <alban.ktz@gmail.com>
2022-02-23 14:50:38 +0100

    Markers relied to the json files + add automaticly

--- testmap-withouttsx/src/components/Marqueur/Marqueur.js
+++ testmap-withouttsx/src/components/Marqueur/Marqueur.js
@@ -1,15 +1,25 @@
 import React from "react";
 import "./Marqueur.css";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
+function Marqueur (props) {
 
-function Marqueur(props) {
+  function checkPhone() {
+    if (props.tel.length > 0) {
+      return props.tel;
+    } else {
+      return "";
+    }
+  }
+  
   return (
-    <Marker className="marqueur" position={props.position}>
+    
+    <Marker className={"marqueur" + props.type_lieux} key={props.key} position={props.position}>
       <Popup>
         <h2>{props.nom}</h2>
         <p>{props.des}</p>
+        <p>{checkPhone()}</p>
       </Popup>
     </Marker>
   );
