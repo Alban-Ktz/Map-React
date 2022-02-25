3db6cc7b22d9fcdfd29aa3c8a1797eadac65e3a5
Alban Ktz <alban.ktz@gmail.com>
2022-02-25 02:12:47 +0100

    css settings

--- testmap-withouttsx/src/pages/SettingsPage/SettingsPage.js
+++ testmap-withouttsx/src/pages/SettingsPage/SettingsPage.js
@@ -1,6 +1,8 @@
 import "./SettingsPage.css";
 import GoToMapBtn from "../../components/GoToMapBtn/GoToMapBtn";
 import VibrationSetting from "../../components/VibrationSetting/VibrationSetting";
+import RayonSetting from "../../components/RayonSetting/RayonSetting";
+import Darkmod from "../../components/Darkmod/Darkmod";
 
 
 function SettingsPage() {
@@ -8,22 +10,13 @@ function SettingsPage() {
     <>
       <GoToMapBtn />
       <div className="settingsPage">
+
         <h1>Settings</h1>
+
         <VibrationSetting />
-        <ul className="white">
-          <li>
-            <select name="radius" id="radius-select">
-              <option value="10">10 mètres</option>
-              <option value="20">20 mètres</option>
-              <option value="30">30 mètres</option>
-              <option value="40">40 mètres</option>
-              <option value="50">50 mètres</option>
-              <option value="60">60 mètres</option>
-              <option value="70">70 mètres</option>
-            </select>
-          </li>
-          <li>Choisir un rayon de vibration</li>
-        </ul>
+        <RayonSetting />
+        <Darkmod />
+
       </div>
     </>
   );
